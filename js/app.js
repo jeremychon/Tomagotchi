78eaef09401293014ae866c1db131fdd5b0b6f28
Jeremy Chon <Jeremy@Jeremys-MacBook-Pro.local>
2019-07-02 14:54:33 -0500

    created Tomagotchi class

---
+++ js/app.js
@@ -0,0 +1,13 @@
+console.log('hey');
+
+class Tomagotchi {
+	constructor (name) {
+		this.name = name;
+		this.age = 0;
+		this.hunger = 0;
+		this.boredom = 0;
+		this.sleepiness = 0;
+	}
+}
+
+const tomagotchi = new Tomagotchi();
