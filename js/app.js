3f7f6b276a0a5b4555de298d7529be0bab7952bc
Jeremy Chon <Jeremy@Jeremys-MacBook-Pro.local>
2019-07-02 15:36:00 -0500

    able to give name with input field

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 console.log('hey');
 
-class Tomagotchi {
+class Tamagotchi {
 	constructor (name) {
 		this.name = name;
 		this.age = 0;
@@ -12,7 +12,46 @@ class Tomagotchi {
 
 
 const game = {
-	createPlayer () {
-		const tomagotchi = new Tomagotchi();
+	createPet (name) {
+		this.clearInputField();
+
+		// give the pet a name
+		const tamagotchi = new Tamagotchi(name);
+		console.log(tamagotchi);
+
+		$('#name').append(tamagotchi.name)
+
+
+	},
+
+	clearInputField() {
+		$('#inputName').val('')
 	}
 }
+
+// game object will run the game
+
+// create a tomagotchi pet to play with
+
+	// picture of tomagotchi will appear (hopefully moving)
+	// tomagotchi will have a name, age, 
+		// hunger, boredom, and sleepiness
+
+// pet's age will increase over time
+	// pet will change appearance over time
+// pet's hunger boredom and sleepiness will increase over time
+// pet dies if any of these 3 become 10
+// 
+
+
+
+
+// listeners
+// listens to the form to add the name of the pet
+$('#addName').on('submit', (e) => {
+	e.preventDefault();
+	const $input = $(e.target[0]).val()
+	game.createPet($input)
+})
+
+// feed pet, turn off lights, play with pet
